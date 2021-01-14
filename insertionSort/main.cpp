67267df04f6daf1e4aafffb42b2311af63c8d152
Sudipto Ghosh <sudipto@ghosh.pro>
2021-01-14 13:11:40 +0530

    add results and plot

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -1,12 +1,17 @@
 /**
- * Write a Program to implement Insertion Sort.
+ * Write a Program to implement Insertion Sort and report
+ * the number of comparisons made.
  * 
  * Written by Sudipto Ghosh for the University of Delhi
  */
 
 #include <cstdlib>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+
+#define MIN_SIZE 30
+#define MAX_SIZE 1000
 
 using namespace std;
 
@@ -14,54 +19,84 @@ int insertionSort(int *, int);
 
 int main()
 {
-    int size, comparisons;
-    cout << "Enter array size: ";
-    cin >> size;
-    cout << endl;
+    try
+    {
+        srand(time(0));
 
-    int array[size];
-    
-    // Worst Case
-    cout << "Worst Case:\n------------\n";
-    for (int i = 0; i < size; i++)
-        array[i] = size - i;
-    comparisons = insertionSort(array, size);
-    cout << "Total Comparisons Made: "
-         << comparisons << endl
-         << endl;
-    
-    // Best Case
-    cout << "Best Case:\n------------\n";
-    for (int i = 0; i < size; i++)
-        array[i] = i + 1;
-    comparisons = insertionSort(array, size);
-    cout << "Total Comparisons Made: "
-         << comparisons << endl
-         << endl;
-    
-    // Average Case
-    cout << "Average Case:\n------------\n";
-    ifstream fin("./random.txt");
-    for (int i = 0; i < size; i++)
-        fin >> array[i];
-    fin.close();
-    comparisons = insertionSort(array, size);
-        cout << "Total Comparisons Made: "
-         << comparisons << endl
-         << endl;
-    
-    return 0;
+        int array[MAX_SIZE];
+        int size, comparisons;
+
+        ofstream fout("results.csv");
+
+        cout << "+------------------------------------------------+\n";
+        cout << "| Input Size | Best Case | Avg Case | Worst Case |\n";
+        cout << "+------------------------------------------------+\n";
+
+        fout << "size,best,avg,worst\n";
+
+        for (int i = 0; i < 100; i++)
+        {
+            // rand() % (upperBound + 1 - lowerBound) + lowerBound
+            size = rand() % (MAX_SIZE + 1 - MIN_SIZE) + MIN_SIZE;
+
+            // Input Size
+            cout << "| " << setw(10) << size;
+            fout << size << ",";
+
+            // Best Case
+            for (int i = 0; i < size; i++)
+                array[i] = i + 1;
+            comparisons = insertionSort(array, size);
+            cout << " | " << setw(9) << right << comparisons;
+            fout << comparisons << ",";
+
+            // Average Case
+            try
+            {
+                ifstream fin("./random.txt");
+                for (int i = 0; i < size; i++)
+                    fin >> array[i];
+                fin.close();
+                comparisons = insertionSort(array, size);
+                cout << " | " << setw(8) << right << comparisons;
+                fout << comparisons << ",";
+            }
+            catch (exception e)
+            {
+                cerr << e.what();
+                return -1;
+            }
+
+            // Worst Case
+            for (int i = 0; i < size; i++)
+                array[i] = size - i;
+            comparisons = insertionSort(array, size);
+            cout << " | " << setw(10) << right << comparisons << " |\n";
+            fout << comparisons << "\n";
+        }
+
+        cout << "+------------------------------------------------+\n\n";
+
+        fout.close();
+
+        return 0;
+    }
+    catch (exception e)
+    {
+        cerr << e.what();
+        return -1;
+    }
 }
 
 int insertionSort(int *array, int size)
 {
     int i, j, k, key, iterCompCount, count = 0;
-    
+
     for (i = 1; i < size; i++)
     {
         key = array[i];
         iterCompCount = 0;
-        
+
         for (j = i - 1; j >= 0; j--)
         {
             count++;
@@ -79,6 +114,6 @@ int insertionSort(int *array, int size)
 
         array[j + 1] = key;
     }
-    
+
     return count;
 }
